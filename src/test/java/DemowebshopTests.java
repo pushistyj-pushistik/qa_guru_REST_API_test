8df0b95da3247c51e33299e530174353f7bfb3a3
zhanna sencenko <jannas93@mail.ru>
2022-06-24 00:56:03 +0300

    added demowebshop tests + allure report

---
+++ src/test/java/DemowebshopTests.java
@@ -0,0 +1,79 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+
+import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.Cookie;
+
+
+public class DemowebshopTests {
+    static String login = "qaguru@qa.guru",
+            password = "qaguru@qa.guru1",
+            authCookieName = "NOPCOMMERCE.AUTH";
+
+    @BeforeAll
+    static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.baseUrl = "http://demowebshop.tricentis.com";
+        RestAssured.baseURI = "http://demowebshop.tricentis.com";
+    }
+
+    @AfterEach
+    void afterEach() {
+        closeWebDriver();
+    }
+
+    @Test
+    @DisplayName("Successful authorization to some demowebshop (UI)")
+    void loginTest() {
+        step("Open login page", () ->
+                open("/login"));
+
+        step("Fill login form", () -> {
+            $("#Email").setValue(login);
+            $("#Password").setValue(password)
+                    .pressEnter();
+        });
+
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    }
+
+    @Test
+    @DisplayName("Successful authorization to some demowebshop (API + UI)")
+    void loginWithApiTest() {
+        step("Get cookie by api and set it to browser", () -> {
+            String authCookieValue = given()
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+                    .log().all()
+                    .when()
+                    .post("/login")
+                    .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract().cookie(authCookieName);
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+            step("Set cookie to to browser", () -> {
+                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            });
+        });
+
+        step("Open main page", () ->
+                open(""));
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    }
+}
